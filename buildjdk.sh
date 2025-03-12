faf6c833659808ce32a95d7d4e9b86217a3628cf
dontknowhy <3365304317@qq.com>
2025-03-12 20:47:21 +0800

    delete version string

--- buildjdk.sh
+++ buildjdk.sh
@@ -91,7 +91,6 @@ bash ./configure \
     --with-debug-level="release" \
     --with-version-pre="FoldCraftLauncher" \
     --with-vendor-name="dontknowhy" \
-    --with-version-string="MT8797" \
     --openjdk-target=$TARGET \
     --with-extra-cflags="$CFLAGS" \
     --with-extra-cxxflags="$CFLAGS" \
