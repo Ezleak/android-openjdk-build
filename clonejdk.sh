1f6f8ce18d90c32ed21d348a262138eabebd6b06
Ezleak <116900642+Ezleak@users.noreply.github.com>
2026-01-25 19:09:13 +0800

    dragonwell

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-git clone --depth 1 --branch jdk25 https://github.com/graalvm/labs-openjdk openjdk
+git clone --depth 1 https://github.com/dragonwell-project/dragonwell25 openjdk
+#--branch jdk25 https://github.com/graalvm/labs-openjdk openjdk
