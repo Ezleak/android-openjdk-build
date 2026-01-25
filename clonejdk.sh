1239134c5b3ce97db9132dd724f35b7eb16d58e1
Ezleak <116900642+Ezleak@users.noreply.github.com>
2026-01-25 19:37:24 +0800

    Update GitHub repository URL for cloning OpenJDK

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-git clone --depth 1 https://github.com/corretto/corretto-25 openjdk
+git clone --depth 1 https://github.com/openjdk/jdk25u-dev openjdk
 #--branch jdk25 https://github.com/graalvm/labs-openjdk openjdk
